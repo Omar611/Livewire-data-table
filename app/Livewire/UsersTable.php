c21d1ad6b7b07e52fdc1f04a9a82a1b9ac01885b
Omar Tarek <omartarek711@gmail.com>
2023-09-23 22:11:28 +0300

    Add dynamic paginated users

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+    use WithPagination;
+
+    #[Rule(['perPage' => 'required|integer|min:1'])]
+    public $perPage = 10;
+
     public function render()
     {
-        return view('livewire.users-table');
+        return view('livewire.users-table', [
+            'users' => User::paginate($this->perPage),
+        ]);
     }
 }
